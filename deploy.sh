54e920e78c95e97433bc81bd71c4c578fd735ffd
Ilya <38791099+Onexx@users.noreply.github.com>
2022-12-20 11:33:48 +0300

    Add more output to deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@
 docker image build . -t backend:latest
 docker tag backend:latest onexx/backend:latest
 docker push onexx/backend:latest
+echo ------------------
+echo Deployed successfully, now you need to run ./deploybackend on the server
 read -p "Press any key to resume ..."
